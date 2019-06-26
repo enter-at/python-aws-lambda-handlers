f339414c4b56eb8cd485c7a79c60ace04a6f8d0c
Alexandre Savio <alexsavio@gmail.com>
2019-06-26 11:04:09 +0200

    test(formatters): fix tests

--- lambda_handlers/formatters/tests/test_json_formatter.py
+++ lambda_handlers/formatters/tests/test_json_formatter.py
@@ -3,7 +3,7 @@ from typing import Dict, Union
 
 import pytest
 
-from lambda_handlers.formatters import InputFormat, OutputFormat
+from lambda_handlers.formatters import input_format, output_format
 
 
 @pytest.fixture
@@ -18,43 +18,43 @@ def data_sample() -> Dict[str, Union[str, int, bool]]:
 class TestInputFormatJSON:
 
     def test_valid(self, data_sample):
-        assert data_sample == InputFormat.json(json.dumps(data_sample))
+        assert data_sample == input_format.json(json.dumps(data_sample))
 
     def test_empty_content(self):
-        assert {} == InputFormat.json('{}')
+        assert {} == input_format.json('{}')
 
     def test_random_string(self):
-        with pytest.raises(json.decoder.JSONDecodeError, match='Expecting value:'):
-            InputFormat.json('sadfawfopq2yr923')
+        with pytest.raises(json.decoder.JSONDecodeError, match=r'Expecting value: line 1 column 1 \(char 0\)'):
+            input_format.json('sadfawfopq2yr923')
 
     def test_empty_string(self):
-        with pytest.raises(json.decoder.JSONDecodeError, match='Expecting value:'):
-            InputFormat.json('')
+        with pytest.raises(json.decoder.JSONDecodeError, match=r'Expecting value: line 1 column 1 \(char 0\)'):
+            input_format.json('')
 
     def test_none(self):
-        with pytest.raises(TypeError, match='the JSON object must be'):
-            InputFormat.json(None)
+        with pytest.raises(TypeError, match='the JSON object must be str, bytes or bytearray, not NoneType'):
+            input_format.json(None)
 
 
 class TestOutputFormatJSON:
 
     def test_valid(self, data_sample):
-        assert json.dumps(data_sample) == OutputFormat.json(data_sample)
+        assert json.dumps(data_sample) == output_format.json(data_sample)
 
     def test_empty_content(self):
-        assert '{}' == OutputFormat.json({})
+        assert '{}' == output_format.json({})
 
     def test_random_string(self):
-        assert '"sadfawfopq2yr923"' == OutputFormat.json('sadfawfopq2yr923')
+        assert '"sadfawfopq2yr923"' == output_format.json('sadfawfopq2yr923')
 
     def test_empty_string(self):
-        assert '""' == OutputFormat.json('')
+        assert '""' == output_format.json('')
 
     def test_none(self):
-        assert 'null' == OutputFormat.json(None)
+        assert 'null' == output_format.json(None)
 
     def test_bool(self):
-        assert 'true' == OutputFormat.json(True)
+        assert 'true' == output_format.json(True)
 
     def test_int(self):
-        assert '42' == OutputFormat.json(42)
+        assert '42' == output_format.json(42)
